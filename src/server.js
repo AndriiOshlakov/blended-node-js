8d0201a081d8baaa48a2194c514e28aa2cfa9a8c
Artem <tem4ik20.1@gmail.com>
2025-10-04 12:01:35 +0300

    add new request

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import { connectMongoDB } from './db/connectMongoDB.js';
 import { errorHandler } from './middleware/errorHandler.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { logger } from './middleware/logger.js';
-
+import productsRouters from './routes/productsRoutes.js';
 const app = express();
 const PORT = process.env.PORT ?? 3030;
 
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(cors());
 app.use(logger);
 
+app.use(productsRouters);
 app.use(notFoundHandler);
 app.use(errorHandler);
 
